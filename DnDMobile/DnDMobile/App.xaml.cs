85841720e22d001ed02506dbe49846fbc43266bc
Xiokraze <xiokraze@gmail.com>
2020-06-07 12:01:19 -0500

    Added new styles.

--- DnDMobile/DnDMobile/App.xaml.cs
+++ DnDMobile/DnDMobile/App.xaml.cs
@@ -10,7 +10,7 @@ namespace DnDMobile
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
