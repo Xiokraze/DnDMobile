9f71512dcaf994d04647b25d915e23f28b236dcd
Xiokraze <xiokraze@gmail.com>
2020-06-06 21:21:28 -0500

    Added splash activity with image.

---
+++ DnDMobile/DnDMobile.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace DnDMobile.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+        }
+    }
+}
