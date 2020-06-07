49b0db9c03a466a59575ce02e9005704f74c5edf
Xiokraze <xiokraze@gmail.com>
2020-06-07 11:58:38 -0500

    Added initial user class.

---
+++ DnDMobile/DnDMobile/Classes/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string VerificationPassword { get; set; }
+
+        //private readonly int maxInputLength = 255;
+
+
+        public User() { }
+
+
+    }
+}
