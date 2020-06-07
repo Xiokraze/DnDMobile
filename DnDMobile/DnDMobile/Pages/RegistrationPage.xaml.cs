152183a7636b6aa21de371378effd253ffc151f3
Xiokraze <xiokraze@gmail.com>
2020-06-07 11:58:11 -0500

    Added new user registration page.

---
+++ DnDMobile/DnDMobile/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DnDMobile.Classes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDMobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+            PageStack.FadeTo(1, 2000);
+        }
+
+
+        private async void LogoutClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+
+
+        private async void ButtonRegisterProcedure(object sender, EventArgs e)
+        {
+            ButtonRegister.IsEnabled = false;
+            User user = new User()
+            {
+                Username = EntryUsername.Text,
+                Password = EntryPassword.Text,
+                VerificationPassword = EntryConfirmPassword.Text
+            };
+
+            if (EnteredData(user))
+            {
+
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Enter all data.", "OK");
+            }
+
+            ButtonRegister.IsEnabled = true;
+        }
+
+
+        private bool EnteredData(User user)
+        {
+            bool enteredUsername = !string.IsNullOrEmpty(user.Username);
+            bool enteredPassword = !string.IsNullOrEmpty(user.Password);
+            bool enteredConfirmPassword = !string.IsNullOrEmpty(user.VerificationPassword);
+            return enteredUsername && enteredPassword && enteredConfirmPassword;
+        }
+    }
+}
