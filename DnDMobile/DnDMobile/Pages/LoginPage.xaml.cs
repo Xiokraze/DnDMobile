cd0d650d345f6b705945f9e79db6362515c5f58e
Xiokraze <xiokraze@gmail.com>
2020-06-06 21:22:43 -0500

    Renamed MainPage to LoginPage. Added GUI layout and initial button handlers to login page.

---
+++ DnDMobile/DnDMobile/Pages/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DnDMobile
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            PageStack.FadeTo(1, 2000);
+        }
+
+
+        private async void ButtonLoginProcedure(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "In Development", "Ok");
+        }
+
+
+        private async void ButtonRegisterProcedure(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "In Development", "Ok");
+        }
+    }
+}
