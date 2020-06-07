c195baa562fc908f071fff7c9fd3fa285eca1c49
Xiokraze <xiokraze@gmail.com>
2020-06-07 12:36:48 -0500

    Added class for using the dependency service to display toast messages.

---
+++ DnDMobile/DnDMobile/Classes/IToast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    interface IToast
+    {
+        void Show(string message);
+    }
+}
---
+++ DnDMobile/DnDMobile/Classes/ToastAndroid.cs
@@ -0,0 +1,15 @@
+﻿using DnDMobile.Classes;
+using Android.Widget;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ToastAndroid))]
+namespace DnDMobile.Classes
+{
+
+    class ToastAndroid : IToast
+    {
+        public void Show(string message)
+        {
+            Android.Widget.Toast.MakeText(Android.App.Application.Context, message, ToastLength.Long).Show();
+        }
+    }
+}
