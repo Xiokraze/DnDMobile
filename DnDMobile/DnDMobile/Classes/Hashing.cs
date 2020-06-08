d6079a453bacef824df22ac1dd4f4bc095f6c838
Xiokraze <xiokraze@gmail.com>
2020-06-07 20:17:58 -0500

    Added class for encrypting passwords.

---
+++ DnDMobile/DnDMobile/Classes/Hashing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    class Hashing
+    {
+        private const int ITERATIONS = 10000;
+        private const int HASH_SIZE = 20;
+        private const int SALT_SIZE = 16;
+        private const int BYTE_SIZE = HASH_SIZE + SALT_SIZE;
+
+
+        // Utilize PBKDF2 encryption with salt and hashing to convert a typed password into a string of characters.
+        public string RegistrationHash(string text)
+        {
+            // Generate random salt.
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SALT_SIZE]);
+
+            // Hash and salt the password using PBKDF2.
+            var pbdkdf2 = new Rfc2898DeriveBytes(text, salt, ITERATIONS);
+            byte[] hash = pbdkdf2.GetBytes(HASH_SIZE);
+
+            // Create byte array to hold salt+hashed values.
+            byte[] hashBytes = new byte[BYTE_SIZE];
+            Array.Copy(salt, 0, hashBytes, 0, SALT_SIZE);
+            Array.Copy(hash, 0, hashBytes, SALT_SIZE, HASH_SIZE);
+
+            // Convert salt+hashed byte array to a string.
+            string pwHash = Convert.ToBase64String(hashBytes);
+            return pwHash;
+        }
+    }
+
+
+
+}
