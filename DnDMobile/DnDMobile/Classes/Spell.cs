18bf195321edfaa02c9613e87f64b871456c8a26
Xiokraze <xiokraze@gmail.com>
2020-07-20 22:01:02 -0500

    Added class for Spells (fetching spell list from the text file) and Spell (parameters for each spell)

---
+++ DnDMobile/DnDMobile/Classes/Spell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    public class Spell
+    {
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public string CastingTime { get; set; }
+        public string Range { get; set; }
+        public string Components { get; set; }
+        public string Duration { get; set; }
+        public string School { get; set; }
+        public string Attack { get; set; }
+        public string DamageEffect { get; set; }
+        public string Ritual { get; set; }
+        public string Concentration { get; set; }
+        public string Source { get; set; }
+        public string AtHigherLevel { get; set; }
+        public string CastableBy { get; set; }
+        public string Description { get; set; }
+
+
+        // Constructor
+        public Spell() { }
+    }
+}
---
+++ DnDMobile/DnDMobile/Classes/Spells.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace DnDMobile.Classes
+{
+    class Spells
+    {
+        public List<Spell> SpellList { get; } = SetSpellList();
+
+
+        public Spells() { }
+
+
+        public List<Spell> GetSpellList()
+        {
+            return SpellList;
+        }
+
+
+        private static List<Spell> SetSpellList()
+        {
+            List<Spell> SpellList = new List<Spell>();
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "DnDMobile.spells.txt";
+                string line;
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        try
+                        {
+                            Spell spell = JsonConvert.DeserializeObject<Spell>(line);
+                            if (spell != null)
+                            {
+                                SpellList.Add(spell);
+                            }
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+            catch { }
+            return SpellList;
+        }
+    }
+}
