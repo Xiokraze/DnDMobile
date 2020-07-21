908cd1d82e17dfd3a6bb140dff0e1c5ae3d613b5
Xiokraze <xiokraze@gmail.com>
2020-07-20 22:05:53 -0500

    Added page for displaying selected spell data.

---
+++ DnDMobile/DnDMobile/Pages/SpellInfoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using DnDMobile.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDMobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SpellInfoPage : ContentPage
+    {
+        public SpellInfoPage(Spell spell)
+        {
+            InitializeComponent();
+            BindingContext = spell;
+        }
+    }
+}
