ccf6e4014e92aeba478fdd1692fa2fd47f12b007
Xiokraze <xiokraze@gmail.com>
2020-08-19 20:35:09 -0500

    Added classes for armor and weapons.

---
+++ DnDMobile/DnDMobile/Classes/Armor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    class Armor
+    {
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public string ArmorClass { get; set; }
+        public string RequiredStrength { get; set; }
+        public string StealthImpact { get; set; }
+        public string Weight { get; set; }
+
+        // Consctructor.
+        public Armor(string description, string value, string armorClass, string requiredStrength, string stealthImpact, string weight) 
+        {
+            this.Description = description;
+            this.Value = value;
+            this.ArmorClass = armorClass;
+            this.RequiredStrength = requiredStrength;
+            this.StealthImpact = stealthImpact;
+            this.Weight = weight;
+        }
+    }
+}
---
+++ DnDMobile/DnDMobile/Classes/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    class Weapon
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public string Damage { get; set; }
+        public string Weight { get; set; }
+        public string Properties { get; set; }
+
+
+        // Constructor.
+        public Weapon(string type, string value, string damage, string weight, string properties)
+        {
+            this.Type = type;
+            this.Value = value;
+            this.Damage = damage;
+            this.Weight = weight;
+            this.Properties = properties;
+        }
+    }
+}
