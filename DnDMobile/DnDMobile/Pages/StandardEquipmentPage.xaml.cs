1e2102fc4c86b65a4a04a557740fd5f50cc6f9cc
Xiokraze <xiokraze@gmail.com>
2020-08-19 20:47:49 -0500

    Added page for displaying standard equipment.

---
+++ DnDMobile/DnDMobile/Pages/StandardEquipmentPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDMobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StandardEquipmentPage : ContentPage
+    {
+        public StandardEquipmentPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonShowArmor(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void ButtonHideArmor(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
