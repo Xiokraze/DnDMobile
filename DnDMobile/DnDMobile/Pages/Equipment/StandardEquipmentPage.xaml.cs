0ba5946787190261a4d07e8594904e56483d6123
Xiokraze <xiokraze@gmail.com>
2020-09-12 18:57:38 -0500

    StandardEquipmentPage: Moved files to new folder. Added class as a navigation menu to keep equipment types organized.

---
+++ DnDMobile/DnDMobile/Pages/Equipment/StandardEquipmentPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDMobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StandardEqupimentPage : ContentPage
+    {
+        public StandardEqupimentPage()
+        {
+            InitializeComponent();
+            PageStack.FadeTo(1, 2000);
+        }
+
+
+        private async void ButtonViewStandardWeaponsProcedure(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StandardWeaponsPage());
+        }
+
+
+        private async void ButtonViewStandardArmorProcedure(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StandardArmorPage());
+        }
+    }
+}
