ea8b3901af3cdf0ee619749fe6271d44c55211ed
Xiokraze <xiokraze@gmail.com>
2020-09-29 21:20:07 -0500

    Added two new classes.

---
+++ DnDMobile/DnDMobile/Classes/ItemsFolder/MenuListViewItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDMobile.Classes.ItemsFolder
+{
+    class MenuListViewItem
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+        public MenuListViewItem(string name, int count)
+        {
+            this.Name = name;
+            this.Count = count;
+        }
+    }
+}
