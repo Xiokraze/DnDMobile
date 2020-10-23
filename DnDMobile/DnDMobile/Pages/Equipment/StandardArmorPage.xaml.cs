02ddd4c5aaa953f9b1db988bf613851ddf0a07bb
Xiokraze <xiokraze@gmail.com>
2020-10-23 16:50:41 -0500

    StandardArmorPage: Added horizontal options and refactored the GUI of the page to utilize lists.

--- DnDMobile/DnDMobile/Pages/Equipment/StandardArmorPage.xaml.cs
+++ DnDMobile/DnDMobile/Pages/Equipment/StandardArmorPage.xaml.cs
@@ -9,197 +9,94 @@ namespace DnDMobile.Pages.Equipment
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StandardArmorPage : ContentPage
     {
-        GUIHelper guiHelper = new GUIHelper();
-        private static readonly Items items = new Items();
-        bool pageLoad = true;
+        int rowNumber = 0;
+        Grid grid;
+
 
         public StandardArmorPage()
         {
             InitializeComponent();
             PageStack.FadeTo(1, 2000);
             InitializeItemLists();
-            pageLoad = false;
-        }
-
-
-        private void OnItemsSearchTextChanged(object sender, EventArgs e)
-        {
-            SearchBar searchBar = (SearchBar)sender;
-            string searchText = searchBar.Text.ToLower();
-            if (string.IsNullOrEmpty(searchText))
-            {
-                pageLoad = true;
-            }
-            else
-            {
-                pageLoad = false;
-            }
-            InitializeItemLists(searchText);
         }
 
 
         private void InitializeItemLists(string searchText = "")
         {
-            InitializeArmor(searchText, items.GetStandardLightArmor(), LightArmorGrid);
-            InitializeArmor(searchText, items.GetStandardMediumArmor(), MediumArmorGrid);
-            InitializeArmor(searchText, items.GetStandardHeavyArmor(), HeavyArmorGrid);
-            InitializeArmor(searchText, items.GetStandardShields(), ShieldsGrid);
+            grid = StandardArmorGrid;
+            Items items = new Items();
+            InitializeArmor(items.GetStandardLightArmor(), "Light Armor");
+            InitializeArmor(items.GetStandardMediumArmor(), "Medium Armor");
+            InitializeArmor(items.GetStandardHeavyArmor(), "Heavy Armor");
+            InitializeArmor(items.GetStandardShields(), "Shields");
         }
 
 
-        private void ShowGrid(string gridID)
-        {
-            switch (gridID)
-            {
-                case "IDLightArmor":
-                    ButtonShowLightArmorProcedure(null, null);
-                    break;
-                case "IDMediumArmor":
-                    ButtonShowMediumArmorProcedure(null, null);
-                    break;
-                case "IDHeavyArmor":
-                    ButtonShowHeavyArmorProcedure(null, null);
-                    break;
-                case "IDShields":
-                    ButtonShowShieldsProcedure(null, null);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-
-        private void HideGrid(string gridID)
-        {
-            switch (gridID)
-            {
-                case "IDLightArmor":
-                    ButtonHideLightArmorProcedure(null, null);
-                    break;
-                case "IDMediumArmor":
-                    ButtonHideMediumArmorProcedure(null, null);
-                    break;
-                case "IDHeavyArmor":
-                    ButtonHideHeavyArmorProcedure(null, null);
-                    break;
-                case "IDShields":
-                    ButtonHideShieldsProcedure(null, null);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-
-        private void ButtonShowLightArmorProcedure(object sender, EventArgs e)
-        {
-            guiHelper.ShowElementsProcedure(ButtonShowLightArmor, ButtonHideLightArmor, LabelLightArmorHeader, ScrollviewLightArmor);
-        }
-
-
-        private void ButtonHideLightArmorProcedure(object sender, EventArgs e)
-        {
-            guiHelper.HideElementsProcedure(ButtonShowLightArmor, ButtonHideLightArmor, LabelLightArmorHeader, ScrollviewLightArmor);
-        }
-
-
-        private void ButtonShowMediumArmorProcedure(object sender, EventArgs e)
-        {
-            guiHelper.ShowElementsProcedure(ButtonShowMediumArmor, ButtonHideMediumArmor, LabelMediumArmorHeader, ScrollviewMediumArmor);
-        }
-
-
-        private void ButtonHideMediumArmorProcedure(object sender, EventArgs e)
-        {
-            guiHelper.HideElementsProcedure(ButtonShowMediumArmor, ButtonHideMediumArmor, LabelMediumArmorHeader, ScrollviewMediumArmor);
-        }
-
-
-        private void ButtonShowHeavyArmorProcedure(object sender, EventArgs e)
-        {
-            guiHelper.ShowElementsProcedure(ButtonShowHeavyArmor, ButtonHideHeavyArmor, LabelHeavyArmorHeader, ScrollviewHeavyArmor);
-        }
-
-
-        private void ButtonHideHeavyArmorProcedure(object sender, EventArgs e)
-        {
-            guiHelper.HideElementsProcedure(ButtonShowHeavyArmor, ButtonHideHeavyArmor, LabelHeavyArmorHeader, ScrollviewHeavyArmor);
-        }
-
-
-        private void ButtonShowShieldsProcedure(object sender, EventArgs e)
-        {
-            guiHelper.ShowElementsProcedure(ButtonShowShields, ButtonHideShields, LabelShieldsHeader, ScrollviewShields);
-        }
-
-
-        private void ButtonHideShieldsProcedure(object sender, EventArgs e)
-        {
-            guiHelper.HideElementsProcedure(ButtonShowShields, ButtonHideShields, LabelShieldsHeader, ScrollviewShields);
-        }
-
-
-        public void CreateWeaponGridLabel(string text, int row, int col, Grid grid, bool isHeader = false)
+        public void CreateWeaponGridLabel(string text, int column, bool isHeader = false)
         {
             Label label = new Label()
             {
                 Text = text,
                 Style = (Style)Application.Current.Resources["labelGridDataStyle"],
-                HorizontalOptions = LayoutOptions.Start
+                HorizontalOptions = LayoutOptions.Center
             };
             if (isHeader)
             {
                 label.TextColor = Color.FromHex("#930C10");
             }
-            Grid.SetRow(label, row);
-            Grid.SetColumn(label, col);
+            Grid.SetRow(label, rowNumber);
+            Grid.SetColumn(label, column);
             grid.Children.Add(label);
         }
 
 
-        private void AddArmorGridHeaders(Grid grid)
+        private void AddArmorGridHeaders()
         {
             RowDefinition rowDefinition = new RowDefinition();
-            
             grid.RowDefinitions.Add(rowDefinition);
-            CreateWeaponGridLabel("Description", 0, 0, grid, true);
-            CreateWeaponGridLabel("Value", 0, 1, grid, true);
-            CreateWeaponGridLabel("Armor Class", 0, 2, grid, true);
-            CreateWeaponGridLabel("Required Strength", 0, 3, grid, true);
-            CreateWeaponGridLabel("Weight", 0, 4, grid, true);
+            CreateWeaponGridLabel("Description", 0, true);
+            CreateWeaponGridLabel("Value", 1, true);
+            CreateWeaponGridLabel("Armor Class", 2, true);
+            CreateWeaponGridLabel("Required Strength", 3, true);
+            CreateWeaponGridLabel("Weight", 4, true);
+            rowNumber++;
         }
 
 
-        private void InitializeArmor(string searchDescription, List<Armor> armorList, Grid grid)
+        private void AddLabel(string labelText)
         {
-            grid.RowDefinitions.Clear();
-            grid.Children.Clear();
-            AddArmorGridHeaders(grid);
-            int rowNumber = 1;
-            Boolean matchFound = false;
+            rowNumber++;
+            RowDefinition rowDefinition = new RowDefinition();
+            grid.RowDefinitions.Add(rowDefinition);
+            Label label = new Label()
+            { 
+                Text = labelText,
+                FontSize = 22,
+                Style = (Style)Application.Current.Resources["labelHeaderStyle"]
+        };
+            Grid.SetRow(label, rowNumber);
+            Grid.SetColumnSpan(label, 5);
+            grid.Children.Add(label);
+            rowNumber++;
+        }
+
+
+        private void InitializeArmor(List<Armor> armorList, string labelText)
+        {
+            
+            AddLabel(labelText);
+            AddArmorGridHeaders();
 
             foreach (Armor armor in armorList)
             {
-                if (string.IsNullOrEmpty(searchDescription) || armor.Description.ToLower().Contains(searchDescription.ToLower()))
-                {
-                    matchFound = true;
-                    RowDefinition rowDefinition = new RowDefinition();
-                    grid.RowDefinitions.Add(rowDefinition);
-                    CreateWeaponGridLabel(armor.Description, rowNumber, 0, grid);
-                    CreateWeaponGridLabel(armor.Value, rowNumber, 1, grid);
-                    CreateWeaponGridLabel(armor.ArmorClass, rowNumber, 2, grid);
-                    CreateWeaponGridLabel(armor.RequiredStrength, rowNumber, 3, grid);
-                    CreateWeaponGridLabel(armor.Weight, rowNumber, 4, grid);
-                    rowNumber++;
-                }
-            }
-            if (matchFound && pageLoad != true)
-            {
-                ShowGrid(grid.StyleId);
-            }
-            else
-            {
-                HideGrid(grid.StyleId);
+                RowDefinition rowDefinition = new RowDefinition();
+                grid.RowDefinitions.Add(rowDefinition);
+                CreateWeaponGridLabel(armor.Description, 0);
+                CreateWeaponGridLabel(armor.Value, 1);
+                CreateWeaponGridLabel(armor.ArmorClass, 2);
+                CreateWeaponGridLabel(armor.RequiredStrength, 3);
+                CreateWeaponGridLabel(armor.Weight, 4);
+                rowNumber++;
             }
         }
     }
