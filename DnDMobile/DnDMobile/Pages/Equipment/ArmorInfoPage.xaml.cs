dcf172327422a014c5e8d24407a45962421d60ad
Xiokraze <xiokraze@gmail.com>
2020-10-25 21:22:03 -0500

    Added armor info page.

---
+++ DnDMobile/DnDMobile/Pages/Equipment/ArmorInfoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using DnDMobile.Classes.ItemsFolder;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDMobile.Pages.Equipment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ArmorInfoPage : ContentPage
+    {
+        public ArmorInfoPage(Armor armor)
+        {
+            InitializeComponent();
+            BindingContext = armor;
+        }
+    }
+}
