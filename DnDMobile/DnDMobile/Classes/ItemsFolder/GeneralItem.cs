f8cc9966639f8460dd7f64d9b6d3574150e02ce0
Xiokraze <xiokraze@gmail.com>
2020-10-25 20:52:08 -0500

    GeneralItem: made the class public, renamed classification to type.

--- DnDMobile/DnDMobile/Classes/ItemsFolder/GeneralItem.cs
+++ DnDMobile/DnDMobile/Classes/ItemsFolder/GeneralItem.cs
@@ -1,20 +1,20 @@
 ﻿namespace DnDMobile.Classes.ItemsFolder
 {
-    class GeneralItem
+    public class GeneralItem
     {
         public string Description { get; set; }
         public string Value { get; set; }
         public string Weight { get; set; }
-        public string Classification { get; set; }
+        public string Type { get; set; }
 
 
         // Constructor.
-        public GeneralItem(string description, string value, string weight, string classification)
+        public GeneralItem(string description, string value, string weight, string type)
         {
             this.Description = description;
             this.Value = value;
             this.Weight = weight;
-            this.Classification = classification;
+            this.Type = type;
         }
     }
 }
