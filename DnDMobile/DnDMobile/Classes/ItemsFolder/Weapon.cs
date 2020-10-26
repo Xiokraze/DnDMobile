e34acf9a3d746388a23e6d8ddc6ac7dd3a305399
Xiokraze <xiokraze@gmail.com>
2020-10-25 20:57:55 -0500

    Weapon: Added type and updated constructor.

--- DnDMobile/DnDMobile/Classes/ItemsFolder/Weapon.cs
+++ DnDMobile/DnDMobile/Classes/ItemsFolder/Weapon.cs
@@ -1,22 +1,24 @@
 ﻿namespace DnDMobile.Classes.ItemsFolder
 {
-    class Weapon
+    public class Weapon
     {
         public string Description { get; set; }
         public string Value { get; set; }
         public string Damage { get; set; }
         public string Weight { get; set; }
         public string Properties { get; set; }
+        public string Type { get; set; }
 
 
         // Constructor.
-        public Weapon(string type, string value, string damage, string weight, string properties)
+        public Weapon(string description, string value, string damage, string weight, string properties, string type)
         {
-            this.Description = type;
+            this.Description = description;
             this.Value = value;
             this.Damage = damage;
             this.Weight = weight;
             this.Properties = properties;
+            this.Type = type;
         }
     }
 }
