3b9ca144161c0c825770f013dbbfaf34a46f8750
Xiokraze <xiokraze@gmail.com>
2020-10-25 21:22:43 -0500

    StaticVariables: Updated comments and added armor variables.

--- DnDMobile/DnDMobile/Classes/StaticVariables.cs
+++ DnDMobile/DnDMobile/Classes/StaticVariables.cs
@@ -9,12 +9,19 @@ namespace DnDMobile.Classes
         // General.
         public static string reset = "Reset";
 
-        // Weapon Classifications.
+        // Weapons.
         public static string simpleMelee = "Simple Melee";
         public static string simpleRanged = "Simple Ranged";
         public static string martialMelee = "Martial Melee";
         public static string martialRanged = "Martial Ranged";
         public static string[] meleeTypes = { simpleMelee, simpleRanged, martialMelee, martialRanged, reset };
+
+        //Armor.
+        public static string lightAmor = "Light Armor";
+        public static string mediumAmor = "Medium Armor";
+        public static string heavyAmor = "Heavy Armor";
+        public static string shields = "Shields";
+        public static string[] armorTypes = { lightAmor, mediumAmor, heavyAmor, shields, reset};
 
         // General Items.
         public static string ammunition = "Ammunition";
