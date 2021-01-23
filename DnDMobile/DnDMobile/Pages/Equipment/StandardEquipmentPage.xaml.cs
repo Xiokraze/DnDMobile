b0192f12ad7e7f4635c713503c52551fdb4bdcbc
Xiokraze <xiokraze@gmail.com>
2021-01-23 16:49:24 -0600

    StandardEquipment: Removed unused usings.

--- DnDMobile/DnDMobile/Pages/Equipment/StandardEquipmentPage.xaml.cs
+++ DnDMobile/DnDMobile/Pages/Equipment/StandardEquipmentPage.xaml.cs
@@ -2,10 +2,6 @@
 using DnDMobile.Pages.Equipment;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
