f0c14848e626965a1b60c308c445999e3efdc7b3
Xiokraze <xiokraze@gmail.com>
2021-01-23 16:47:26 -0600

    Added dice roller class.

---
+++ DnDMobile/DnDMobile/Classes/RollDice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    class RollDice
+    {
+        public int RollRandom(int min, int max)
+        {
+            return StaticVariables.randomNumber.Next(min, max);
+        }
+    }
+}
