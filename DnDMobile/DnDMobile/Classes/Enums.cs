3f445f5e3ce336c73ea82d859bd6b46a407d689e
Xiokraze <xiokraze@gmail.com>
2021-01-24 00:28:17 -0600

    Started converting string types and comparisons to enums.

---
+++ DnDMobile/DnDMobile/Classes/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDMobile.Classes
+{
+    public enum ItemType
+    {
+        Ammunition,
+        ArcaneFocus,
+        DruidicFocus,
+        GeneralGood,
+        HeavyArmor,
+        HolySymbol,
+        LightArmor,
+        MartialMelee,
+        MartialRanged,
+        MediumArmor,
+        Reset,
+        Shield,
+        SimpleMelee,
+        SimpleRanged
+    }
+
+
+    public enum GoldValue
+    { 
+        GP10,
+        GP25,
+        GP50,
+        GP100,
+        GP250,
+        GP500,
+        GP750,
+        GP1000,
+        GP2500,
+        GP5000,
+        GP7500
+    }
+
+}
