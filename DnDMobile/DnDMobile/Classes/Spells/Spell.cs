153512e5804a85ca56a402cbc3a1c43f60a76cff
Xiokraze <xiokraze@gmail.com>
2021-01-24 12:23:02 -0600

    Refactored spell classes into their own folder for maintainability.

--- DnDMobile/DnDMobile/Classes/Spells/Spell.cs
+++ DnDMobile/DnDMobile/Classes/Spells/Spell.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
-
-namespace DnDMobile.Classes
+﻿namespace DnDMobile.Classes
 {
     public class Spell
     {
